4635ed75299ed4e064764563bba123facab3b2de
pasit nusso <montionugera@gmail.com>
2017-08-17 15:32:41 +0700

    init project structure

--- yellow-ios/yellow/AppDelegate.swift
+++ yellow-ios/yellow/AppDelegate.swift
@@ -12,10 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var initialViewController :UIViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = HomeVC()
+        homeViewController.view.backgroundColor = UIColor.red
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
         return true
     }
 

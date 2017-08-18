cea28dc24dd0edae9104ac3da7f6d39dbc3bdb9e
ekachai <ekachai@23perspective.com>
2017-08-18 12:04:30 +0700

    add Geohash lib

--- yellow-ios/yellow/HomeVC.swift
+++ yellow-ios/yellow/HomeVC.swift
@@ -14,6 +14,20 @@ class HomeVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        if let (lat, lon) = Geohash.decode(hash: "u4pruydqqvj") {
+            print(lat)
+            print(lon)
+            // lat.min == 57.649109959602356
+            // lat.max == 57.649111300706863
+            // lon.min == 10.407439023256302
+            // lon.max == 10.407440364360809
+        }
+        
+        let s = Geohash.encode(latitude: 57.64911063015461, longitude: 10.40743969380855, length: 10)
+        // s == "u4pruydqqv"
+
+        
     }
 
     override func didReceiveMemoryWarning() {

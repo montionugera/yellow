9a2d923756c0173ddd6370e346d36b0d379f1ecb
pasit nusso <montionugera@gmail.com>
2017-08-24 13:33:03 +0700

    Integrate with firebase / post video to firebase storage success / try to login with firebase on home page success

---
+++ yellow-ios/BusinessService.swift
@@ -0,0 +1,13 @@
+//
+//  BusinessService.swift
+//  yellow
+//
+//  Created by montionugera on 8/23/17.
+//  Copyright © 2017 23. All rights reserved.
+//
+
+import UIKit
+
+class BusinessService: NSObject {
+
+}
---
+++ yellow-ios/PostContent.swift
@@ -0,0 +1,9 @@
+//
+//  PostContent.swift
+//  yellow
+//
+//  Created by montionugera on 8/23/17.
+//  Copyright © 2017 23. All rights reserved.
+//
+
+import Foundation
